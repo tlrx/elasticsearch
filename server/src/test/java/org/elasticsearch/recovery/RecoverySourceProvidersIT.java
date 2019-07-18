d52984d74fbf842d51d03912856da8c57f838be0
Tanguy Leroux <tlrx.dev@gmail.com>
2019-07-18 14:32:45 +0200

    add test with corrupted primary

--- server/src/test/java/org/elasticsearch/recovery/RecoverySourceProvidersIT.java
+++ server/src/test/java/org/elasticsearch/recovery/RecoverySourceProvidersIT.java
@@ -18,8 +18,11 @@
  */
 package org.elasticsearch.recovery;
 
+import org.apache.lucene.index.CorruptIndexException;
 import org.elasticsearch.Version;
 import org.elasticsearch.action.admin.cluster.snapshots.create.CreateSnapshotResponse;
+import org.elasticsearch.action.search.SearchResponse;
+import org.elasticsearch.cluster.ClusterState;
 import org.elasticsearch.cluster.metadata.IndexMetaData;
 import org.elasticsearch.cluster.routing.IndexRoutingTable;
 import org.elasticsearch.cluster.routing.RecoverySource;
@@ -28,16 +31,22 @@ import org.elasticsearch.cluster.routing.ShardRouting;
 import org.elasticsearch.cluster.routing.UnassignedInfo;
 import org.elasticsearch.common.settings.Setting;
 import org.elasticsearch.common.settings.Settings;
-import org.elasticsearch.common.unit.TimeValue;
+import org.elasticsearch.env.Environment;
+import org.elasticsearch.index.Index;
+import org.elasticsearch.index.IndexService;
+import org.elasticsearch.indices.IndicesService;
 import org.elasticsearch.plugins.ClusterPlugin;
 import org.elasticsearch.plugins.Plugin;
 import org.elasticsearch.rest.RestStatus;
 import org.elasticsearch.snapshots.Snapshot;
 import org.elasticsearch.snapshots.SnapshotId;
 import org.elasticsearch.snapshots.SnapshotInfo;
+import org.elasticsearch.test.CorruptionUtils;
 import org.elasticsearch.test.ESIntegTestCase;
 import org.elasticsearch.test.InternalTestCluster;
+import org.elasticsearch.test.store.MockFSIndexStore;
 
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Collections;
@@ -54,6 +63,7 @@ import static org.elasticsearch.common.settings.Setting.simpleString;
 import static org.elasticsearch.common.settings.Setting.versionSetting;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertAcked;
 import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertHitCount;
+import static org.elasticsearch.test.hamcrest.ElasticsearchAssertions.assertNoFailures;
 import static org.hamcrest.Matchers.equalTo;
 import static org.hamcrest.Matchers.hasSize;
 
@@ -77,6 +87,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
     protected Collection<Class<? extends Plugin>> nodePlugins() {
         Collection<Class<? extends Plugin>> classes = new ArrayList<>(super.nodePlugins());
         classes.add(RecoverySourceProvidersIT.TestPlugin.class);
+        classes.add(MockFSIndexStore.TestPlugin.class);
         return classes;
     }
 
@@ -128,7 +139,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         final String dataNode = internalCluster().startDataOnlyNode();
 
         final int numberOfDocs = scaledRandomIntBetween(1, 100);
-        final String index = createRestoredIndex(1, 0, numberOfDocs);
+        final String index = createRestoredIndex(1, 0, numberOfDocs, true);
         ensureGreen(index);
 
         final int extraDocs = scaledRandomIntBetween(10, 50);
@@ -154,7 +165,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         assertThat(indexRoutingTable.numberOfNodesShardsAreAllocatedOn(dataNode), equalTo(1));
 
         // indicates that the shard recovered from the snapshot (does not contain the extra docs)
-        assertHitCount(client().prepareSearch(index).setSize(0).get(), numberOfDocs);
+        assertDocsCount(index, numberOfDocs);
     }
 
     public void testAutoRestorePrimaryAfterMultipleNodesLeft() throws Exception {
@@ -162,7 +173,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         final List<String> dataNodes = internalCluster().startDataOnlyNodes(randomIntBetween(2, 5));
 
         final int numberOfDocs = scaledRandomIntBetween(1, 100);
-        final String index = createRestoredIndex(randomIntBetween(dataNodes.size(), 10), 0, numberOfDocs);
+        final String index = createRestoredIndex(randomIntBetween(dataNodes.size(), 10), 0, numberOfDocs, true);
         ensureGreen(index);
 
         final List<String> stoppedNodes = randomSubsetOf(randomIntBetween(1, dataNodes.size() - 1), dataNodes);
@@ -170,16 +181,18 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
             internalCluster().stopRandomNode(InternalTestCluster.nameFilter(node));
         }
 
+        ensureStableCluster(1 + dataNodes.size() - stoppedNodes.size());
+
         // NORELEASE Fix this
         // we might need something in RoutingNodes#ignoreShard(ShardRouting, AllocationStatus, IndexMetaData, RoutingChangesObserver)
         // to trigger the reroute when a recovery source has changed?
         //
         // need to manually trigger a reroute
         assertAcked(client().admin().cluster().prepareReroute());
-        ensureGreen(TimeValue.timeValueSeconds(5), index);
+        ensureGreen(index);
 
         // indicates that the shard recovered from the snapshot (does not contain the extra docs)
-        assertHitCount(client().prepareSearch(index).setSize(0).get(), numberOfDocs);
+        assertDocsCount(index, numberOfDocs);
     }
 
     public void testAutoRestorePrimaryAfterFullRestart() throws Exception {
@@ -187,7 +200,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         internalCluster().startDataOnlyNode();
 
         final int numberOfDocs = scaledRandomIntBetween(1, 100);
-        final String index = createRestoredIndex(1, 0, numberOfDocs);
+        final String index = createRestoredIndex(1, 0, numberOfDocs, true);
         ensureGreen(index);
 
         final int extraDocs = scaledRandomIntBetween(10, 50);
@@ -210,7 +223,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         // but there is no delayed allocation for primaries, so the shard always recovers from the snapshot
         // and does not contain the extra docs
         //assertHitCount(client().prepareSearch(index).setSize(0).get(), numberOfDocs + extraDocs);
-        assertHitCount(client().prepareSearch(index).setSize(0).get(), numberOfDocs);
+        assertDocsCount(index, numberOfDocs);
     }
 
     public void testAutoRestorePrimaryAfterFullRestartAndNewDataNodes() throws Exception {
@@ -218,7 +231,7 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         internalCluster().startDataOnlyNode();
 
         final int numberOfDocs = scaledRandomIntBetween(1, 100);
-        final String index = createRestoredIndex(1, 0, numberOfDocs);
+        final String index = createRestoredIndex(1, 0, numberOfDocs, true);
         ensureGreen(index);
 
         final int extraDocs = scaledRandomIntBetween(10, 50);
@@ -235,24 +248,53 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
         internalCluster().startMasterOnlyNode(masterNodeSettings);
         internalCluster().startDataOnlyNode();
 
-        ensureGreen(TimeValue.timeValueSeconds(3), index);
+        ensureGreen(index);
 
         IndexRoutingTable indexRoutingTable = client().admin().cluster().prepareState().get().getState().routingTable().index(index);
         assertThat(indexRoutingTable.primaryShardsActive(), equalTo(1));
 
         // indicates that the shard recovered from the snapshot (does not contain the extra docs)
-        assertHitCount(client().prepareSearch(index).setSize(0).get(), numberOfDocs);
+        assertDocsCount(index, numberOfDocs);
     }
 
-    // + test avec failed shard
+    public void testAutoRestoreFailedPrimary() throws Exception {
+        internalCluster().startMasterOnlyNode();
+        internalCluster().startDataOnlyNodes(2);
+
+        final int numberOfDocs = scaledRandomIntBetween(1, 100);
+        final String indexName = createRestoredIndex(2, 0, numberOfDocs, false); // no check on close as we corrupt the index
+        ensureGreen(indexName);
+
+        assertHitCount(client().prepareSearch(indexName).setSize(0).get(), numberOfDocs);
+
+        ClusterState clusterState = client().admin().cluster().prepareState().get().getState();
+        IndexRoutingTable indexRoutingTable = clusterState.routingTable().index(indexName);
+        final Index index = indexRoutingTable.getIndex();
+        final ShardRouting shardRouting = indexRoutingTable.shard(0).primaryShard();
+        final String shardNodeName = clusterState.nodes().resolveNode(shardRouting.currentNodeId()).getName();
+
+        // corrupt the index on disk
+        Environment environment = internalCluster().getInstance(Environment.class, shardNodeName);
+        Path indexDataPath = environment.dataFiles()[0].resolve("indices").resolve(index.getUUID())
+            .resolve(String.valueOf(shardRouting.getId())).resolve("index");
+        CorruptionUtils.corruptIndex(random(), indexDataPath, true);
+
+        // fail the shard (notifies the master and triggers a reroute)
+        IndexService indexService = internalCluster().getInstance(IndicesService.class, shardNodeName).indexServiceSafe(index);
+        indexService.getShard(0).failShard("test", new CorruptIndexException("test", "index is corrupted"));
+
+        assertBusy(() -> assertDocsCount(indexName, numberOfDocs));
+    }
 
     private String createRestoredIndex(final int numberOfShards,
                                        final int numberOfReplicas,
-                                       final int numberOfDocs) throws Exception {
+                                       final int numberOfDocs,
+                                       final boolean checkOnClose) throws Exception {
         final String index = randomAlphaOfLength(10).toLowerCase(Locale.ROOT);
         createIndex(index, Settings.builder()
             .put(IndexMetaData.SETTING_NUMBER_OF_SHARDS, numberOfShards)
             .put(IndexMetaData.SETTING_NUMBER_OF_REPLICAS, numberOfReplicas)
+            .put(MockFSIndexStore.INDEX_CHECK_INDEX_ON_CLOSE_SETTING.getKey(), checkOnClose)
             .build());
 
         if (numberOfDocs > 0) {
@@ -299,5 +341,11 @@ public class RecoverySourceProvidersIT extends ESIntegTestCase {
 
         ensureGreen(restoredIndex);
         return restoredIndex;
+    }
+
+    private static void assertDocsCount(final String indexName, final long expectedNumberOfDocs) {
+        SearchResponse searchResponse = client().prepareSearch(indexName).setSize(0).get();
+        assertHitCount(searchResponse, expectedNumberOfDocs);
+        assertNoFailures(searchResponse);
     }
 }
