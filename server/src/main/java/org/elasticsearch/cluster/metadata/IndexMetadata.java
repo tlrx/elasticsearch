885a23a1a50c0907567b01cbe6d82d60eb06a4c1
tlrx <tlrx.dev@gmail.com>
2025-12-10 15:16:16 +0100

    Revert "Enable synthetic id by default"
    This reverts commit 8ff7c5b1a5a796b81d91539373d7fa8355e599d9.

--- server/src/main/java/org/elasticsearch/cluster/metadata/IndexMetadata.java
+++ server/src/main/java/org/elasticsearch/cluster/metadata/IndexMetadata.java
@@ -2563,7 +2563,11 @@ public class IndexMetadata implements Diffable<IndexMetadata>, ToXContentFragmen
             if (isTsdb
                 && IndexSettings.TSDB_SYNTHETIC_ID_FEATURE_FLAG
                 && indexCreatedVersion.onOrAfter(IndexVersions.TIME_SERIES_USE_SYNTHETIC_ID)) {
-                useTimeSeriesSyntheticId = true;
+                var setting = settings.get(IndexSettings.USE_SYNTHETIC_ID.getKey());
+                if (setting != null && setting.equalsIgnoreCase(Boolean.TRUE.toString())) {
+                    assert IndexSettings.TSDB_SYNTHETIC_ID_FEATURE_FLAG;
+                    useTimeSeriesSyntheticId = true;
+                }
             }
             return new IndexMetadata(
                 new Index(index, uuid),
--- server/src/main/java/org/elasticsearch/index/IndexSettings.java
+++ server/src/main/java/org/elasticsearch/index/IndexSettings.java
@@ -1249,7 +1249,7 @@ public final class IndexSettings {
         useTimeSeriesDocValuesFormat = scopedSettings.get(USE_TIME_SERIES_DOC_VALUES_FORMAT_SETTING);
         useTimeSeriesDocValuesFormatLargeBlockSize = scopedSettings.get(USE_TIME_SERIES_DOC_VALUES_FORMAT_LARGE_BLOCK_SIZE);
         useEs812PostingsFormat = scopedSettings.get(USE_ES_812_POSTINGS_FORMAT);
-        final var useSyntheticId = IndexSettings.TSDB_SYNTHETIC_ID_FEATURE_FLAG && indexMetadata.getIndexMode() == IndexMode.TIME_SERIES;
+        final var useSyntheticId = IndexSettings.TSDB_SYNTHETIC_ID_FEATURE_FLAG && scopedSettings.get(USE_SYNTHETIC_ID);
         if (indexMetadata.useTimeSeriesSyntheticId() != useSyntheticId) {
             assert false;
             throw new IllegalArgumentException(
