a438def2a4ff6d763b5b88010cf087991bdfd8fb
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2025-12-15 10:14:45 +0100

    Skip invert

--- server/src/main/java/org/elasticsearch/common/lucene/uid/VersionsAndSeqNoResolver.java
+++ server/src/main/java/org/elasticsearch/common/lucene/uid/VersionsAndSeqNoResolver.java
@@ -76,7 +76,7 @@ public final class VersionsAndSeqNoResolver {
         if (lookupState == null) {
             lookupState = new PerThreadIDVersionAndSeqNoLookup[reader.leaves().size()];
             for (LeafReaderContext leaf : reader.leaves()) {
-                lookupState[leaf.ord] = new PerThreadIDVersionAndSeqNoLookup(leaf.reader(), loadTimestampRange);
+                lookupState[leaf.ord] = new PerThreadIDVersionAndSeqNoLookup(new IDReader(leaf.reader()), loadTimestampRange);
             }
             ctl.set(lookupState);
         } else {
