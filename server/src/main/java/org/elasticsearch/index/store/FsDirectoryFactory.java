205b645db1c2f3713a59f30a329c199ae748721c
Francisco Fernández Castaño <francisco.fernandez.castano@gmail.com>
2025-12-15 11:12:35 +0100

    Try random

--- server/src/main/java/org/elasticsearch/index/store/FsDirectoryFactory.java
+++ server/src/main/java/org/elasticsearch/index/store/FsDirectoryFactory.java
@@ -158,7 +158,7 @@ public class FsDirectoryFactory implements IndexStorePlugin.DirectoryFactory {
                 return Optional.of(ReadAdvice.NORMAL);
             }
             if (name.endsWith(".cfs")) {
-                return Optional.of(ReadAdvice.NORMAL);
+                return Optional.of(ReadAdvice.RANDOM);
             }
             if (name.endsWith(".sfbf")) {
                 return Optional.of(ReadAdvice.RANDOM);
